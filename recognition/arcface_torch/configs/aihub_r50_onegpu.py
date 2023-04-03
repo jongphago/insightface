0365f040a29ffdd719e01f954d19c41c77338e8c
jongphago <jonghyun.d.kim@gmail.com>
2023-04-03 10:52:22 +0000

    🚀 feat(validation_function_test.ipynb): wandb를 사용하여 실험 로깅 추가 wandb를 사용하여 실험 로깅을 추가했습니다. 이제 실험 결과를 더 쉽게 추적하고 비교할 수 있습니다. 또한, 일부 하드 코딩된 값들을 config 파일에서 가져오도록 변경하였습니다. 마지막으로, `return_aihub_dataloader` 함수를 사용하여 데이터 로딩을 더 간단하게 구현하였습니다.
    🚀 feat(utils_config.py): return_aihub_dataloader 정의
    이미지 쌍 파일 경로를 반환하는 return_pairs_path() 함수와 AIHubDataset을 사용하여 DataLoader를 생성하는 return_aihub_dataloader() 함수를 정의하였습니다.
    
    🔧 수정(aihub_r50_onegpu.py): dataloader를 정의 하기 위한 파라미터를 config 파일로 이동
    dataloader를 정의 하기 위한 파라미터를 config 파일로 이동하였습니다. 이제 config 파일에서 데이터셋 경로, 배치 사이즈, 쓰레드 수 등을 설정할 수 있습니다.

--- recognition/arcface_torch/configs/aihub_r50_onegpu.py
+++ recognition/arcface_torch/configs/aihub_r50_onegpu.py
@@ -1,4 +1,5 @@
 from easydict import EasyDict as edict
+from utils.utils_config import return_pairs_path
 
 # make training faster
 # our RAM is 256G
@@ -20,6 +21,7 @@ config.verbose = 2000
 config.dali = False
 config.save_all_states = True
 config.pretrained = True
+config.dropout = 0.0
 
 config.rec = "/home/jupyter/data/face-image/train_aihub_family"
 config.num_classes = 2154
@@ -27,3 +29,27 @@ config.num_image = 93006
 config.num_epoch = 20
 config.warmup_epoch = 0
 config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
+
+config.task = "family"
+
+config.path = edict()
+path = config.path
+path.work_dirs = "/home/jongphago/insightface/work_dirs"
+path.checkpoint = ""
+
+config.data = edict()
+config.data.image_size = 112
+config.data.aihub_mean = [0.5444, 0.4335, 0.3800]
+config.data.aihub_std = [0.2672, 0.2295, 0.2156]
+config.data.pairs_path = return_pairs_path # function
+
+config.data.test = edict()
+config.data.test.path = edict()
+
+test = config.data.test
+test.split = "test"
+test.task = "family"
+test.batch_size = 200
+test.num_workers = 4
+test.path.aihub_dataroot = "/home/jupyter/insightface/data/face-image/test_aihub_family"
+config.data.test.path.pairs_path = config.data.pairs_path
--- recognition/arcface_torch/utils/utils_config.py
+++ recognition/arcface_torch/utils/utils_config.py
@@ -1,9 +1,16 @@
 import importlib
 import os.path as osp
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+from easydict import EasyDict
+from datasets.AIHubDataset import AIHubDataset
 
 
 def get_config(config_file):
-    assert config_file.startswith('configs/'), 'config file setting must start with configs/'
+    assert config_file.startswith(
+        "configs/"
+    ), "config file setting must start with configs/"
     temp_config_name = osp.basename(config_file)
     temp_module_name = osp.splitext(temp_config_name)[0]
     config = importlib.import_module("configs.base")
@@ -12,5 +19,50 @@ def get_config(config_file):
     job_cfg = config.config
     cfg.update(job_cfg)
     if cfg.output is None:
-        cfg.output = osp.join('work_dirs', temp_module_name)
+        cfg.output = osp.join("work_dirs", temp_module_name)
     return cfg
+
+
+def return_pairs_path(config_data_split: dict) -> str:
+    """
+    주어진 split과 task에 해당하는 파일 경로를 반환합니다.
+
+    :param config_data_split: 데이터셋의 분할 유형과 수행할 작업에 대한 설명을 포함한 data config dict
+    :return: 파일 경로를 포함한 문자열
+    :rtype: str
+    :raises FileNotFoundError: 파일이 존재하지 않을 때 발생하는 예외
+    """
+
+    def _cap(s: str) -> str:
+        return s[0].upper() + s[1:]
+
+    file_path = f"/home/jupyter/insightface/data/pairs/{config_data_split.split}/pairs_{_cap(config_data_split.task)}.txt"
+
+    if not osp.exists(file_path):
+        raise FileNotFoundError(f"파일이 존재하지 않습니다: {file_path}")
+
+    return file_path
+
+
+def return_aihub_dataloader(
+    config_data_split: EasyDict, transform: Compose
+) -> DataLoader:
+    """
+    주어진 설정과 데이터 변환을 사용하여 DataLoader를 생성합니다.
+
+    :param config_data_split: 데이터 분할에 대한 설정이 담긴 EasyDict 객체
+    :param transform: 이미지 데이터에 적용할 데이터 변환 객체 (예: torchvision.transforms.Compose)
+    :return: 생성된 DataLoader 객체
+    :rtype: torch.utils.data.DataLoader
+    """
+    aihub_dataloader = torch.utils.data.DataLoader(
+        dataset=AIHubDataset(
+            dir=config_data_split.path.aihub_dataroot,
+            pairs_path=config_data_split.path.pairs_path(config_data_split),
+            transform=transform,
+        ),
+        batch_size=config_data_split.batch_size,
+        num_workers=config_data_split.num_workers,
+        shuffle=False,
+    )
+    return aihub_dataloader
